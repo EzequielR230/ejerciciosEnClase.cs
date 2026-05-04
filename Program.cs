b576dcb4ec88e7dcdbfad39d6b76c4b08930fb38
Ezequiel <cereyesr@uamv.edu.ni>
2026-05-04 12:16:40 -0600

    ejercicioWhile1

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿int numero;
+
+while (true)
+{
+    Console.WriteLine("ingrese un numero entero");
+    if (int.TryParse(Console.ReadLine(), out  numero))
+    {
+        break;
+    }
+    else
+    {
+        Console.ForegroundColor=ConsoleColor.Red;
+        Console.WriteLine("Entrada no valida, por favor; Ingrese un numero entero.");
+        Console.ResetColor();
+    }
+}
+
+Console.WriteLine("Mostrando el cudrado de los antecesores de " + numero+ ";");
+int cont =1;
+while( cont < numero)
+{
+    Console.WriteLine("El cuadrado de" + cont+ "es: " + Math.Pow(cont,2));
+    cont ++;
+}

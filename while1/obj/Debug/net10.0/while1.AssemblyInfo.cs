4d7a2290211b0875882b56493f7c000776dfd4b5
Ezequiel <cereyesr@uamv.edu.ni>
2026-05-06 12:10:48 -0600

    feat: add arreglo1 project

--- while1/obj/Debug/net10.0/while1.AssemblyInfo.cs
+++ while1/obj/Debug/net10.0/while1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("while1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b576dcb4ec88e7dcdbfad39d6b76c4b08930fb38")]
 [assembly: System.Reflection.AssemblyProductAttribute("while1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("while1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
